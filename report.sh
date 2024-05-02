47655e395eaffcadc1a11ff828f7fd355cae7c91
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-02 09:26:17 +0200

    Create report.sh

---
+++ report.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+version=$(cat ~/aethir/log/main-core-api.log | grep "\"ver\" : " | head -1 | awk '{print $3}')
+service=$(sudo systemctl status aethir --no-pager | grep "active (running)" | wc -l)
+chain="testnet"
+id=aethir-$AETHIR_ID
+bucket=node
+
+if [ $service -ne 1 ]
+then 
+  status="error";
+  message="service not running"
+else 
+  status="ok";
+fi
+
+cat << EOF
+{
+  "id":"$id",
+  "machine":"$MACHINE",
+  "chain":"$chain",
+  "type":"node",
+  "status":"$status",
+  "message":"$message",
+  "service":$service,
+  "updated":"$(date --utc +%FT%TZ)"
+}
+EOF
+
+# send data to influxdb
+if [ ! -z $INFLUX_HOST ]
+then
+ curl --request POST \
+ "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$bucket&precision=ns" \
+  --header "Authorization: Token $INFLUX_TOKEN" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Accept: application/json" \
+  --data-binary "
+    status,node=$id,machine=$MACHINE status=\"$status\",message=\"$message\",version=\"$version\",url=\"$url\",chain=\"$chain\" $(date +%s%N) 
+    "
+fi
