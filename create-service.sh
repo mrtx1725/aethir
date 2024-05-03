bdd32d3fe1d8ff500134e36d0ac62cf0223379ab
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-03 13:47:40 +0200

    Update create-service.sh

--- create-service.sh
+++ create-service.sh
@@ -2,12 +2,13 @@ printf "[Unit]
 Description=Aethir Node
 After=network-online.target
 [Service]
-User=$USER
-WorkingDirectory=$HOME/aethir
+User=root
+WorkingDirectory=/root//aethir
 ExecStart=/root/aethir/AethirChecker
 Restart=on-failure
 RestartSec=60
 LimitNOFILE=65535
+Environment=PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/root/aethir
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/aethird.service
 
